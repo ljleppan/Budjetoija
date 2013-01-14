9d45afb93b7eb8b5c2fea1d4616d090d4aacf8ef
ljleppan <leppanen.leo@gmail.com>
2013-01-14 11:53:10 +0200

    projekti ehjatty ja alustettu

--- Budjetoija/src/budjetoija/Budjetoija.java
+++ Budjetoija/src/budjetoija/Budjetoija.java
@@ -14,6 +14,6 @@ public class Budjetoija {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testi");
+        System.out.println("budjetoija");
     }
 }
