ed4ae7efa33dcb03595abf139e70428d3bb619fa
ljleppan <leo.leppanen@helsinki.fi>
2013-01-28 15:43:10 +0200

    Aloitettu GUI:n rakentaminen

--- Budjetoija/src/main/java/budjetoija/Budjetoija.java
+++ Budjetoija/src/main/java/budjetoija/Budjetoija.java
@@ -1,8 +1,21 @@
 package budjetoija;
 
+import budjetoija.kayttoliittyma.GUI;
+import budjetoija.logiikka.Summa;
+import budjetoija.logiikka.Tili;
+import budjetoija.logiikka.Tilitapahtuma;
+import java.util.GregorianCalendar;
+
 public class Budjetoija {
 
     public static void main(String[] args) {
-        System.out.println("Budjetoija");
+        Tili tili = new Tili("Tili");
+        
+        tili.lisaaTilitapahtuma(new Tilitapahtuma("t1", new Summa(100), new GregorianCalendar()));
+        tili.lisaaTilitapahtuma(new Tilitapahtuma("t2", new Summa(200), new GregorianCalendar()));
+        tili.lisaaTilitapahtuma(new Tilitapahtuma("t3", new Summa(300), new GregorianCalendar()));
+        
+        GUI ui = new GUI(tili);
+        java.awt.EventQueue.invokeLater(ui);
     }
 }
