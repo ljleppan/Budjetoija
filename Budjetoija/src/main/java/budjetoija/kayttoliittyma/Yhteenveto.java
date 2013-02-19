959f1cdde581f8825be2546c59476f7ef6e40090
ljleppan <leppanen.leo@gmail.com>
2013-02-19 14:08:22 +0200

    parannettu edellisen jakson tunnistusta

--- Budjetoija/src/main/java/budjetoija/kayttoliittyma/Yhteenveto.java
+++ Budjetoija/src/main/java/budjetoija/kayttoliittyma/Yhteenveto.java
@@ -1,68 +1,62 @@
 package budjetoija.kayttoliittyma;
 
-import budjetoija.logiikka.Paivamaara;
-import budjetoija.logiikka.Summa;
-import budjetoija.logiikka.Tili;
-import budjetoija.logiikka.ToistuvaTilitapahtuma;
-import budjetoija.logiikka.YksittainenTilitapahtuma;
+import budjetoija.logiikka.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
  * Valmistaa tekstimuotoisia yhteenvetoja tilin tapahtumista.
  */
-
 public class Yhteenveto {
-    /** Tili josta yhteenveto tehdään. */
+
+    /**
+     * Tili josta yhteenveto tehdään.
+     */
     Tili tili;
-    
+
     /**
      * Luokan konstruktori.
+     *
      * @param tili tili josta yhteenveto tehdään.
      * @see Tili
      */
-    public Yhteenveto(Tili tili){
+    public Yhteenveto(Tili tili) {
         this.tili = tili;
     }
-    
-    public void setTili(Tili tili){
+
+    public void setTili(Tili tili) {
         this.tili = tili;
     }
-    
-    public Tili getTili(){
+
+    public Tili getTili() {
         return this.tili;
     }
-    
+
     /**
      * Palauttaa String-muotoisen yhteenvedon tilin tapahtumista.
-     * @param alku  Aikavälin alku.
+     *
+     * @param alku Aikavälin alku.
      * @param loppu Aikavälin loppu.
-     * @return  String-muotoinen yhteenveto.
+     * @return String-muotoinen yhteenveto.
      */
-    public String yhteenvetoAikavalilta(Paivamaara alku, Paivamaara loppu){
+    public String yhteenvetoAikavalilta(Paivamaara alku, Paivamaara loppu) {
         //Otetaan kloonit, jotta päivämäärien käsittely ei vaikuta metodin ulkopuolella.
         Paivamaara alkuPvm = (Paivamaara) alku.clone();
         Paivamaara loppuPvm = (Paivamaara) loppu.clone();
-        
+
         Paivamaara edellisenJaksonLoppu = (Paivamaara) alku.clone();
-        Paivamaara edellisenJaksonAlku = new Paivamaara();
-        long jaksonPituus = loppuPvm.getTimeInMillis() - alkuPvm.getTimeInMillis();
-        edellisenJaksonAlku.setTimeInMillis(alkuPvm.getTimeInMillis() - jaksonPituus);
-        
-        //Ei haluta alkupvm:ää sekä alku että loppu saldoihin.
         edellisenJaksonLoppu.add(Calendar.DAY_OF_MONTH, -1);
-        edellisenJaksonAlku.add(Calendar.DAY_OF_MONTH, -1);
         
+        Paivamaara edellisenJaksonAlku = maaritaEdellisenJaksonAlku(alkuPvm, loppuPvm);
+
         Summa tamanSaldoAlussa = laskeSaldo(alkuPvm);
-        alkuPvm.add(Calendar.DAY_OF_MONTH, -1);
         Summa tamanSaldoLopussa = laskeSaldo(loppuPvm);
         Summa tamanSaldonMuutos = new Summa(tamanSaldoLopussa.getSummaInt() - tamanSaldoAlussa.getSummaInt());
-        
+
         Summa edellisenSaldoAlussa = laskeSaldo(edellisenJaksonAlku);
-        edellisenJaksonAlku.add(Calendar.DAY_OF_MONTH, -1);
         Summa edellisenSaldoLopussa = laskeSaldo(edellisenJaksonLoppu);
         Summa edellisenSaldonMuutos = new Summa(edellisenSaldoLopussa.getSummaInt() - edellisenSaldoAlussa.getSummaInt());
-        
+
         return String.format("%s                   %s            %s\n"
                 + "              %s - %s    %s - %s\n"
                 + "%s           %15s            %15s\n"
@@ -78,14 +72,15 @@ public class Yhteenveto {
 
     /**
      * Laskee tilin saldon tietyltä aikaväliltä. Palaute Summa-oliona.
-     * @param alku  Aikavälin alku
+     *
+     * @param alku Aikavälin alku
      * @param loppu Aikavälin loppu
-     * @return  Summa-muotoinen saldo
+     * @return Summa-muotoinen saldo
      */
     public Summa laskeSaldo(Paivamaara alku, Paivamaara loppu) {
         ArrayList<YksittainenTilitapahtuma> tapahtumat = tili.getKaikkiTilitapahtumatAjalta(alku, loppu);
         int muutos = 0;
-        for (YksittainenTilitapahtuma t : tapahtumat){
+        for (YksittainenTilitapahtuma t : tapahtumat) {
             muutos += t.getSumma().getSummaInt();
         }
         return new Summa(muutos);
@@ -93,10 +88,11 @@ public class Yhteenveto {
 
     /**
      * Laskee tilin saldon tiettyyn päivämäärään saakka.
+     *
      * @param loppu Viimeinen päivämäärä joka otetaan huomioon.
      * @return Summa-muotoinen saldo.
      */
-    public Summa laskeSaldo(Paivamaara loppu) { 
+    public Summa laskeSaldo(Paivamaara loppu) {
         int saldo = 0;
         saldo += tilitapahtumienSaldo(loppu);
         saldo += toistuvienTilitapahtumienSaldo(loppu);
@@ -105,35 +101,77 @@ public class Yhteenveto {
 
     /**
      * Laskee yksittäisten tilitapahtumien saldon ennen syötettyä päivämäärää.
+     *
      * @param loppu Viimeinen mukaan otettava päivämäärä.
      * @return Int-muotoinen saldo.
      */
     public int tilitapahtumienSaldo(Paivamaara loppu) {
         int saldo = 0;
         ArrayList<YksittainenTilitapahtuma> tapahtumat = tili.getTilitapahtumat();
-        for (YksittainenTilitapahtuma t : tapahtumat){
-            if (!t.getAikaleima().after(loppu)){
+        for (YksittainenTilitapahtuma t : tapahtumat) {
+            if (!t.getAikaleima().after(loppu)) {
                 saldo += t.getSumma().getSummaInt();
             }
         }
         return saldo;
     }
-    
+
     /**
-     * Laskee toistuvien tilitapahtumien kokonaissaldon ennen syötettyä päivämäärää.
+     * Laskee toistuvien tilitapahtumien kokonaissaldon ennen syötettyä
+     * päivämäärää.
+     *
      * @param loppu Viimeinen mukaan otettava päivämäärä.
      * @return Int-muotoinen saldo.
      */
-    public int toistuvienTilitapahtumienSaldo(Paivamaara loppu){
+    public int toistuvienTilitapahtumienSaldo(Paivamaara loppu) {
         int saldo = 0;
         ArrayList<ToistuvaTilitapahtuma> toistuvatTapahtumat = tili.getToistuvatTilitapahtumat();
-        for (ToistuvaTilitapahtuma tt : toistuvatTapahtumat){
-            ArrayList<YksittainenTilitapahtuma> tapahtumat = tt.konvertoiYksittaisiksiTapahtumiksi(new Paivamaara(1,0,1), loppu);
-            for (YksittainenTilitapahtuma t : tapahtumat){
+        for (ToistuvaTilitapahtuma tt : toistuvatTapahtumat) {
+            ArrayList<YksittainenTilitapahtuma> tapahtumat = tt.konvertoiYksittaisiksiTapahtumiksi(new Paivamaara(1, 0, 1), loppu);
+            for (YksittainenTilitapahtuma t : tapahtumat) {
                 saldo += t.getSumma().getSummaInt();
             }
         }
         return saldo;
     }
-    
+
+    /**
+     * Pyrkii määrittämään edellisen ajanjakson käyttäjälle loogisella tavalla.
+     * Jos alkupvm on jonkin kuukauden ensimmäinen päivä ja loppupvm jonkin kuukauden
+     * viimeinen päivä, on edellinen jakso sama määrä täysiä kuita välittömästi ennen
+     * nykyistä ajanjaksoa, vaikka tämän seurauksena jaksojen välillä olisikin kuiden
+     * pituuksien vaihteluista johtuva päivien määrän ero.
+     * 
+     * Muussa tapauksessa edellinen jakso on nykyistä jaksoa edeltävä jakso, jonka
+     * pituus on yhtä monta päivää kuin nykyisessä jaksossa. Tällöin edellinen jakso
+     * alkaa siten, että sen viimeinen päivä on nykyisen jakson ensimmäistä päivää
+     * edeltävä päivä.
+     * @param alkuPvm Nykyisen jakson ensimmäinen päivä.
+     * @param loppuPvm Nykyisen jakson viimeinen päivä.
+     * @return Päivämäärä-muotoinen esitys edellisen jakson alkupäivästä.
+     */
+    public Paivamaara maaritaEdellisenJaksonAlku(Paivamaara alkuPvm, Paivamaara loppuPvm) {     
+        Paivamaara edellisenJaksonAlku = (Paivamaara) alkuPvm.clone();
+        edellisenJaksonAlku.add(Calendar.DAY_OF_MONTH, -1);
+        
+        if((alkuPvm.get(Calendar.DAY_OF_MONTH) == 1) 
+                && (loppuPvm.get(Calendar.DAY_OF_MONTH) == loppuPvm.getActualMaximum(Calendar.DAY_OF_MONTH))){
+            int vuosienErotus = alkuPvm.get(Calendar.YEAR ) - loppuPvm.get(Calendar.YEAR);
+            edellisenJaksonAlku.add(Calendar.YEAR, vuosienErotus );
+            
+            int kuukausienErotus = alkuPvm.get(Calendar.MONTH) - loppuPvm.get(Calendar.MONTH);
+            edellisenJaksonAlku.add(Calendar.MONTH, kuukausienErotus);
+            
+            edellisenJaksonAlku.set(Calendar.DAY_OF_MONTH, 1);
+        }else{
+            int paivienErotus = 0;
+            Paivamaara loppuClone = (Paivamaara) loppuPvm.clone();
+            while (loppuClone.after(alkuPvm)){
+                loppuClone.add(Calendar.DAY_OF_MONTH, -1);
+                paivienErotus--;
+            }
+            edellisenJaksonAlku.add(Calendar.DAY_OF_MONTH, paivienErotus);
+        }
+        return edellisenJaksonAlku;
+    }
 }
